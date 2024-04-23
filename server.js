d80cf57cc706c64c154445125742a1fe39075115
rjstebner <117238382+rjstebner@users.noreply.github.com>
2024-04-23 11:55:15 +0900

    Setting up the project

--- server.js
+++ server.js
@@ -6,15 +6,19 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
-/* ***********************
- * Routes
- *************************/
-app.use(static)
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
 
+app.use(static)
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home" })
+})
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
