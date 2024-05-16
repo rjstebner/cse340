e7ec8472ddbebd60f89ab337620f6bed263edaad
rjstebner <117238382+rjstebner@users.noreply.github.com>
2024-05-16 10:08:15 +0900

    serverside va,idation working

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,10 +3,18 @@ const express = require("express")
 const router = express.Router() 
 const utilities = require("../utilities")
 const accController = require("../controllers/accController")
+const regValidate = require('../utilities/account-validation')
 
 // Route to handle "My Account" link click
 router.get("/login", utilities.handleErrors(accController.buildLogin))
 router.get('/register', utilities.handleErrors(accController.buildRegister))
-router.post('/register', utilities.handleErrors(accController.registerAccount))
+
+// Process the registration data
+router.post(
+    "/register",
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accController.registerAccount)
+  )
 
 module.exports = router;
