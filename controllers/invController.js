e82e763d4279f3076a87a56753b66e9e24a5a0c4
rjstebner <117238382+rjstebner@users.noreply.github.com>
2024-05-18 09:25:11 +0900

    everything is working

--- controllers/invController.js
+++ controllers/invController.js
@@ -76,7 +76,6 @@ invCont.buildAddClassification = async function (req, res, next) {
 }
 
 invCont.addClassification = async function (req, res) {
-  
   let nav = await utilities.getNav()
   const { classification_name } = req.body
   console.log("classification_name: ", classification_name)
@@ -84,15 +83,13 @@ invCont.addClassification = async function (req, res) {
   const addResult = await invModel.addClassification(classification_name)
   if (addResult) {
     req.flash("notice", "Classification added successfully.")
-    res.status(201).render("./inventory/add-classification", {
-      title: "addClassification",
-      nav,
-    })
+    res.redirect("/inv")
   } else {
     req.flash("notice", "Sorry, the classification was not added.")
     res.status(501).render("./inventory/add-classification", {
       title: "addClassification",
       nav,
+      errors: null,
     })
   }
 }
@@ -110,24 +107,34 @@ invCont.buildAddInventory = async function (req, res, next) {
    })
   }
 
-invCont.addInventory = async function (req, res) {
-  let nav = await utilities.getNav()
-  const { inv_make, inv_model, inv_year, inv_description, inv_image, inv_thumbnail, inv_price, inv_miles, inv_color, classification_id } = req.body
-  console.log("req.body: ", req.body)
-  const addResult = await invModel.addInventory(inv_make, inv_model, inv_year, inv_description, inv_image, inv_thumbnail, inv_price, inv_miles, inv_color, classification_id)
-  if (addResult) {
-    req.flash("notice", "Inventory added successfully.")
-    res.status(201).render("./inventory/add-inventory", {
-      title: "addInventory",
-      nav,
-    })
-  } else {
-    req.flash("notice", "Sorry, the inventory was not added.")
-    res.status(501).render("./inventory/add-inventory", {
-      title: "addInventory",
-      nav,
-    })
+  invCont.addInventory = async function (req, res) {
+    let nav = await utilities.getNav()
+    const {
+      inv_make,
+      inv_model,
+      inv_year,
+      inv_description,
+      inv_image,
+      inv_thumbnail,
+      inv_price,
+      inv_miles,
+      inv_color,
+      classification_id
+    } = req.body
+    console.log("req.body: ", req.body)
+    const addResult = await invModel.addInventory(inv_make, inv_model, inv_year, inv_description, inv_image, inv_thumbnail, inv_price, inv_miles, inv_color, classification_id)
+    let classificationList = await utilities.buildClassificationList()
+    if (addResult) {
+      req.flash("notice", "Inventory added successfully.")
+      res.redirect("/inv")
+    } else {
+      req.flash("notice", "Sorry, the inventory was not added.")
+      res.status(501).render("./inventory/add-inventory", {
+        title: "addInventory",
+        nav,
+        classificationList
+      })
+    }
   }
-}
 
 module.exports = invCont
--- models/inventory-model.js
+++ models/inventory-model.js
@@ -39,6 +39,7 @@ async function getInventoryByInventoryId(inventory_id) {
 async function addInventory(inv_make, inv_model, inv_year, inv_description, inv_image, inv_thumbnail, inv_price, inv_miles, inv_color, classification_id) {
   try{
     const sql = "INSERT INTO public.inventory (inv_make, inv_model, inv_year, inv_description, inv_image, inv_thumbnail, inv_price, inv_miles, inv_color, classification_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *"
+    const data = await pool.query(sql, [inv_make, inv_model, inv_year, inv_description, inv_image, inv_thumbnail, inv_price, inv_miles, inv_color, classification_id])
   } catch (error) {
     console.error("addInventory error " + error)
   }
