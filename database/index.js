c450580cc9a3e87ee3f47c1cb91cdca511aee260
rjstebner <117238382+rjstebner@users.noreply.github.com>
2024-10-02 10:57:43 +0900

    reformated that js and such to try and fix render

--- database/index.js
+++ database/index.js
@@ -1,14 +1,8 @@
-const { Pool } = require("pg")
-require("dotenv").config()
+const { Pool } = require('pg')
+require('dotenv').config()
 
-/* ***************
- * Connection Pool
- * SSL Object needed for local testing of app
- * But will cause problems in production environment
- * If - else will make determination which to use
- * *************** */
 let pool
-if (process.env.NODE_ENV == "development") {
+if (process.env.NODE_ENV === 'development') {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
@@ -18,18 +12,25 @@ if (process.env.NODE_ENV == "development") {
 } else {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
   })
 }
 
-// Added for troubleshooting queries during development
+pool.on('error', (err, client) => {
+  console.error('Unexpected error on idle client', err)
+  process.exit(-1)
+})
+
 module.exports = {
   async query(text, params) {
     try {
       const res = await pool.query(text, params)
-      console.log("executed query", { text })
+      console.log('executed query', { text })
       return res
     } catch (error) {
-      console.error("error in query", { text })
+      console.error('error in query', { text, error })
       throw error
     }
   },
--- server.js
+++ server.js
@@ -1,67 +1,46 @@
-/* ******************************************
- * This server.js file is the primary file of the 
- * application. It is used to control the project.
- *******************************************/
-/* ***********************
- * Require Statements
- *************************/
-const pool = require('./database/')
-const express = require("express")
-const expressLayouts = require("express-ejs-layouts")
-const env = require("dotenv").config()
+const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
+const env = require('dotenv').config()
 const app = express()
-const static = require("./routes/static")
-const baseController = require("./controllers/baseController")
-const inventoryRoute = require("./routes/inventoryRoute")
-const utilities = require("./utilities")
-
-/* ***********************
- * Routes
- *************************/
+const static = require('./routes/static')
+const baseController = require('./controllers/baseController')
+const inventoryRoute = require('./routes/inventoryRoute')
+const utilities = require('./utilities')
 
 // Set the view engine and templates
-
-app.set("view engine", "ejs")
+app.set('view engine', 'ejs')
 app.use(expressLayouts)
-app.set("layout", "layouts/layout")
+app.set('layout', 'layouts/layout')
 
 // Inventory Routes
-app.use("/inv", inventoryRoute)
+app.use('/inv', inventoryRoute)
 
 app.use(static)
 
 // Index Route
-app.get("/", baseController.buildHome)
+app.get('/', baseController.buildHome)
 
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
-  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+  next({ status: 404, message: 'Sorry, we appear to have lost that page.' })
 })
 
-/* ***********************
-* Express Error Handler
-* Place after all other middleware
-*************************/
+// Express Error Handler
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav()
   console.error(`Error at: "${req.originalUrl}": ${err.message}`)
-  res.render("errors/error", {
+  res.render('errors/error', {
     title: err.status || 'Server Error',
     message: err.message,
-    nav
+    nav,
   })
 })
 
-/* ***********************
- * Local Server Information
- * Values from .env (environment) file
- *************************/
+// Local Server Information
 const port = process.env.PORT || 3000
 const host = process.env.HOST || 'localhost'
 
-/* ***********************
- * Log statement to confirm server operation
- *************************/
+// Log statement to confirm server operation
 app.listen(port, host, () => {
   console.log(`app listening on ${host}:${port}`)
 })
